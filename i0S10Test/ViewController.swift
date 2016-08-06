746e5c2e086779109efe7c0defeade9ad7209505
Richard Hughes <richard@richardhughes.com>
2016-08-05 17:41:47 -0700

    Initial Commit

---
+++ i0S10Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  i0S10Test
+//
+//  Created by Richard Hughes on 2016-08-05.
+//  Copyright © 2016 Richard Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
